dfff84209dc98350873b42b8e396690b4f2d0d7f
anari <anarinsk@gmail.com>
2020-12-06 14:23:27 +0900

    first draft /

--- renv.R
+++ renv.R
@@ -3,8 +3,6 @@ install.packages('devtools')
 if (!requireNamespace("remotes"))
   install.packages("remotes")
 
-remotes::install_github("rstudio/renv")
-
 devtools::install_github(c('rstudio/rmarkdown',
                            'yihui/xaringan', 
                            'rstudio/fontawesome', 

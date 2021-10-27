21c0c69d2fdaafebe5f8355690fc3ddef6726898
Junsok Huhh <anarinsk@gmail.com>
2021-10-28 07:19:43 +0900

    fix issues

--- set-renv.R
+++ set-renv.R
@@ -1,5 +1,9 @@
 # Initiate renv
 renv::init(force=T)
+# Do snapshot 
+renv::snapshot()
+# Do restore 
+renv::restore()
 
 # To prevent package errors 
 Sys.setenv(R_INSTALL_STAGED = FALSE)
@@ -18,8 +22,5 @@ devtools::install_github(c('rstudio/rmarkdown',
                            'gadenbuie/xaringanExtra'), force=TRUE)
 
 install.packages('markdown')
-
-# Do snapshot 
-renv::snapshot()
 
 
